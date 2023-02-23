aa352fdde7dd692a3abf4684819ddd5a921e7c33
Mia Jimenez <miajimenez@expediagroup.com>
2023-02-23 16:02:09 +0000

    add validations

--- routers/musiciansRouter.js
+++ routers/musiciansRouter.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const {Musician} = require('../index');
 const {sequelize} = require('../db');
+const {check, validationResult} = require("express-validator");
 
 //TODO
 router.get('/', async (req, res) => {
@@ -17,12 +18,17 @@ router.get('/:id', async (req, res) => {
 
 // express routes
 router.use(express.json());
-router.post('/', async (req, res) => {
-    req.body = {name: "Selena Gomez", instrument: "Voice"};
-    const newMusician = req.body;
-    const createMusician = await Musician.create(newMusician);
-    const allMusicians = await Musician.findAll()
-    res.json(allMusicians);
+router.post('/', [check("name", "instrument").not().isEmpty().trim()], async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        res.json({error: errors.array()})
+    } else {
+        // req.body = {name: "Selena Gomez", instrument: "Voice"};
+        const newMusician = req.body;
+        const createMusician = await Musician.create(newMusician);
+        const allMusicians = await Musician.findAll()
+        res.json(allMusicians);
+    }
 })
 
 router.use(express.json());
